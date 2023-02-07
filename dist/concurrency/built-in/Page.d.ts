685828505ff15e96983db7b217da4ff39c02fead
Calvin Karundu <calvin.mwenda@gmail.com>
2023-02-07 11:56:05 +0300

    Add postinstall script

--- dist/concurrency/built-in/Page.d.ts
+++
@@ -1,7 +0,0 @@
-import { ResourceData } from '../ConcurrencyImplementation';
-import SingleBrowserImplementation from '../SingleBrowserImplementation';
-export default class Page extends SingleBrowserImplementation {
-    private page;
-    protected createResources(): Promise<ResourceData>;
-    protected freeResources(resources: ResourceData): Promise<void>;
-}
